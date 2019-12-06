cb011805ba7631fa1259a32c2c6dabd43dbb024b
demfier <sahu.gaurav719@gmail.com>
2019-12-06 18:40:24 -0500

    Modify perplexity function to parse pickle file

--- src/cli/evaluate.py
+++ src/cli/evaluate.py
@@ -16,20 +16,26 @@ def evaluate():
 
 
 @click.argument('lm_path', type=click.Path(exists=True))
-@click.argument('sample_file', type=click.Path(exists=True))
+@click.argument('pickle_path', type=click.Path(exists=True))
 @evaluate.command()
-def perplexity(lm_path, sample_file):
+def perplexity(lm_path, pickle_path):
     import kenlm
     model = kenlm.LanguageModel(lm_path)
     ppl = Average()
-    num_lines = file_utils.get_num_lines(sample_file)
-    with open(sample_file) as sf:
-        with tqdm(sf, total=num_lines, desc='Computing PPL') as pbar:
+    with open(pickle_path, 'rb') as sf:
+        dialog_dict = pickle.load(sf)
+        num_responses = len(dialog_dict) - 2
+        responses_list = []
+        for rno in range(num_responses):
+            responses = [response for response in dialog_dict[f'response_{rno+1}']]
+            responses_list.append(responses)
+        flattened_responses = [response for responses in responses_list for response in responses]
+        with tqdm(flattened_responses, desc='Computing PPL') as pbar:
             for sentence in pbar:
                 ppl(model.perplexity(sentence))
                 pbar.set_postfix({'PPL': ppl.get_metric()})
 
-    logger.info(f'PPL for file {sample_file} = {ppl.get_metric()}')
+    logger.info(f'PPL for file {pickle_path} = {ppl.get_metric()}')
 
 
 @click.argument('pickle_path', type=click.Path(exists=True))
